98e6c5583c4b369ea9447eb684da1957cf2c1ea9
santanujana12 <40124834+santanujana12@users.noreply.github.com>
2023-12-21 23:21:28 +0530

    Undestanding of React Strictmode

--- src/Components/Counter/counter.jsx
+++ src/Components/Counter/counter.jsx
@@ -1,9 +1,9 @@
-import React,{useReducer} from 'react';
+import React,{useReducer,useState,useEffect} from 'react';
 
 function reducer(state,action){
   switch (action.type){
     case 'INCREMENT':
-      return state+1;
+      return action.payload.count;
     default:
       throw new Error();
   }
@@ -11,14 +11,20 @@ function reducer(state,action){
 
 const Count = () =>{
   const [count,dispatch] = useReducer(reducer,0);
+  const [count2,setCount2] = useState(0);
+
+  useEffect(()=>{
+    setCount2(prev=>prev+1);
+  },[])
 
   function handleClick(){
-    dispatch({type:'INCREMENT',state:1})
+    dispatch({type:'INCREMENT',payload:{count:count+1}})
   }
 
   return (
     <>
       <h1>{count}</h1>
+      <h4>{count2}</h4>
       <button onClick={handleClick}>Click Me!</button>
     </>
   );
