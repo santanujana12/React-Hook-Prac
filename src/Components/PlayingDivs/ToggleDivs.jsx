26af2ebf176ec3b11f2d60796f8d115b373c0f99
santanujana12 <40124834+santanujana12@users.noreply.github.com>
2024-02-04 03:37:42 +0530

    Changed font to caps

--- src/Components/PlayingDivs/ToggleDivs.jsx
+++ src/Components/PlayingDivs/ToggleDivs.jsx
@@ -7,7 +7,7 @@ const TogglingDiv = ()=>{
     <div style={{display:"flex",flexDirection:"column"}}>
       <div style={{height:"150px", width:"150px",backgroundColor:"red",alignSelf:(isToggled?"flex-end":"flex-start")}}/>
       <div style={{height:"150px", width:"150px",backgroundColor:"green",alignSelf:"center",display:"flex"}}>
-        <button style={{alignSelf:"center",marginLeft:"50px",cursor:"pointer"}} onClick={()=>setisToggled(!isToggled)}>Toggle</button>
+        <button style={{alignSelf:"center",marginLeft:"50px",cursor:"pointer"}} onClick={()=>setisToggled(!isToggled)}>TOGGLE</button>
       </div>
       <div style={{height:"150px", width:"150px",backgroundColor:"blue",alignSelf:(isToggled?"flex-start":"flex-end")}}/>
     </div>
