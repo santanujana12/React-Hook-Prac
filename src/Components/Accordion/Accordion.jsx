4fa2084601fad89fb3501cd550bc85dd356ecb95
santanujana12 <40124834+santanujana12@users.noreply.github.com>
2024-02-05 03:51:52 +0530

    1 file modified, 1 added

--- src/Components/Accordion/Accordion.jsx
+++ src/Components/Accordion/Accordion.jsx
@@ -1,6 +1,22 @@
 import React,{useState} from "react";
 
-const Accordion = ()=>{
+// Local imports
+import data from "../../MockData/data";
 
+const Accordion = ()=>{
+  const [index,setIndex] = useState(0);
+
+  return (
+    <>
+      {data.map(item=>{
+        return (
+          <>
+            <h1>{item.title}</h1>
+            <p>{item.values}</p>
+          </>
+        )
+      })}
+    </>
+  );
 }
 export default Accordion;
