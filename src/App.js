88a333ab15bd4c9a02fdcc881164fc686d8c940a
santanujana12 <40124834+santanujana12@users.noreply.github.com>
2024-02-05 03:12:37 +0530

    1 file modified, 1 added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from "react";
+
 import Box from './Components/Box'
 import Count from './Components/Counter/counter';
 import ToDo from "./Components/ToDoApp/ToDo";
 import TogglingDiv from "./Components/PlayingDivs/ToggleDivs";
+import Accordion from "./Components/Accordion/Accordion";
 
 
 export default function App() {
   return (
     <div>
-      <TogglingDiv/>
+      {/* <TogglingDiv/>
+       */}
+       <Accordion/>
     </div>
   );
 }
---
+++ src/Components/Accordion/Accordion.jsx
@@ -0,0 +1,6 @@
+import React,{useState} from "react";
+
+const Accordion = ()=>{
+
+}
+export default Accordion;
