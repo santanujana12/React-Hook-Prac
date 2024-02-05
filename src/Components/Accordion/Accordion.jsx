611d3674894ca423e37b4e55d70e06a4fe5fe14f
santanujana12 <40124834+santanujana12@users.noreply.github.com>
2024-02-05 14:47:58 +0530

    1 file modified

--- src/Components/Accordion/Accordion.jsx
+++ src/Components/Accordion/Accordion.jsx
@@ -1,30 +1,58 @@
-import React,{useState} from "react";
+import React,{useState,useEffect} from "react";
 
 // Local imports
 import data from "../../MockData/data";
 
 const Accordion = ()=>{
   const [dataIndex,setDataIndex] = useState(null);
+  const [allIndex,setAllIndex]=useState([]);
+
+  useEffect(()=>{
+    let newArr = new Array(data.length).fill(false);
+    setAllIndex(newArr);
+  },[])
+
+  // Action based on single index
+  // function handleToggling(index){
+  //   console.log(index)
+  //   if(index===dataIndex){
+  //     setDataIndex(null);
+  //   }else if(index!==dataIndex){
+  //     setDataIndex(index);
+  //   }
+  // }
 
   function handleToggling(index){
-    if(index===dataIndex){
-      setDataIndex(null);
-    }else if(index!==dataIndex){
-      setDataIndex(index);
-    }
+    // One approach
+    // setAllIndex(prevAllIndex=>{
+    //   return prevAllIndex.map((item,idx)=>{
+    //     if(index===idx){
+    //       console.log(item);
+    //       return !item;
+    //     }else{
+    //       return item;
+    //     }
+    //   })
+    // })
+
+    // second approach
+    let newArr = [...allIndex];
+    newArr[index]=!newArr[index];
+    setAllIndex(newArr);
   }
 
 
   return (
     <>
-    {console.log(data.length)}
+    {/* {console.log(allIndex)} */}
       {data.map((item,index)=>{
         return (
-          <div style={{width:"95%"}}>
+          <div key={item.index} style={{width:"95%"}}>
             <h1  className="d-flex flex-row justify-content-between">{item.title}
-              <button onClick={()=>handleToggling(index)}>{dataIndex===index?"-":"+"}</button>
+              <button onClick={()=>handleToggling(index)}>{allIndex[index]?"-":"+"}</button>
             </h1>
-            {dataIndex===index&&<p>{item.values}</p>}
+            {/* {dataIndex===index&&<p>{item.values}</p>} */}
+            {allIndex[index]&&<p>{item.values}</p>}
           </div>
         )
       })}
